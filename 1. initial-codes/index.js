bfe1471f50a4e9bdf9596953061afe546379f7db
Aditya Raj <aditya7raj1@protonmail.com>
2024-05-10 00:28:13 +0530

    closure

--- 1. initial-codes/index.js
+++ 1. initial-codes/index.js
@@ -701,6 +701,7 @@
 // console.log(date.toLocaleTimeString());
 
 // Closure
+// it is a function defined inside of another function, the inner function has access to the variables and scope of the outer function. Allow for private variables and state maintenance. Used frequently in JS frameworks: React, Vue, Angular
 
 function outer() {
   let name = "Aditya";
